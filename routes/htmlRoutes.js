dfe18d8fd7b0fb5bc5230e2fac49992163b03f30
Chelsea Laura <cdesrosiers419@gmail.com>
2019-05-02 18:31:29 -0400

    HTML Routes connected, config fixed

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,27 +1,34 @@
 var db = require("../models");
 
+
 module.exports = function(app) {
-  // Load index page
+
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+      res.render("index");
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
+  app.get("/charters", function(req, res) {
+      res.render("charters", {
+        msg: "Welcome"
       });
-    });
   });
+
+  app.get("/fishingbuddies", function(req, res) {
+      res.render("fishing", {
+        msg: "Welcome"
+      });
+  });
+
+  app.get("/confirmation", function(req, res) {
+    res.render("confirm", {
+      msg: "Welcome"
+    });
+});
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
+  app.get("404", function(req, res) {
+    res.render("404", {
+      msg: "Welcome"
+    });
   });
 };
