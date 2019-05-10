19db30ae272f4b1aa1df1886f600c60aed8a7558
Chelsea Laura <cdesrosiers419@gmail.com>
2019-05-09 20:34:28 -0400

    Routing fixed

--- public/js/trips.js
+++ public/js/trips.js
@@ -1,31 +1,16 @@
-$(".create-form").on("submit", function(event) {
-    event.preventDefault();
+$(".deltrip").on("click", function(event) {
+  // Get the ID from the button.
+  // This is shorthand for $(this).attr("data-tripid")
+  var id = $(this).data("tripid");
 
-    var newTrip = {
-      tripLocation: $("#tripLocation").val().trim(),
-      tripDate: $("#tripDate").val().trim(),
-      description: $("#description").val().trim(),
-      boat: $("#boat").val().trim(),
-      numPeople: $("#numPeople").val().trim(),
-      tripCost: $("#tripCost").val().trim(),
-      image: $("#image").val().trim(),
-    };
-
-    $.post("/api/new", newTrip)
-    // on success, run this callback
-    .then(function(data) {
-      // log the data we found
-      console.log(data);
-      // tell the user we're adding a character with an alert window
-    });
-
-  // empty each input box by replacing the value with an empty string
-  $("#tripLocation").val("");
-  $("#tripDate").val("");
-  $("#description").val("");
-  $("#boat").val("");
-  $("#numPeople").val("");
-  $("#tripCost").val("");
-  $("#image").val("");
-
+  // Send the DELETE request.
+  $.ajax("/trip/" + id, {
+    type: "DELETE"
+  }).then(
+    function() {
+      console.log("deleted id ", id);
+      // Reload the page to get the updated list
+      location.reload();
+    }
+  );
 });
